16dfb26fcb587ed01703b45ec1a501e61832b29f
HuangChangTao <HuangChangTao@yodo1.com>
2020-12-11 10:20:12 +0800

    更工具包

---
+++ k.sh
@@ -0,0 +1,16 @@
+mLog()
+{
+    c_time="$(date +"%Y-%m-%d %H:%M:%S")"
+    echo
+    echo "[${c_time}] ${1}"
+    echo
+}
+CURRENT_PATH="$(cd $(dirname ${BASH_SOURCE:-$0});pwd)"
+
+cd "${CURRENT_PATH}"
+
+mLog "开始自动提交${work_path}"
+git add .
+git commit -m "更工具包"
+git push
+mLog "自动提交结束"
